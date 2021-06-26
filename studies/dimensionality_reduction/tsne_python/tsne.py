31a64eed454b2f46fda1db5a4b1f2c1889ebce94
douglas <douglascardoso@usp.br>
2021-06-26 02:30:42 -0300

    estudo de series temporais, continuo amanha 27-06-21, preciso fazer grafico na funcao interpolate and plot com go.Figure, ainda tem que terminar o modulo

--- studies/dimensionality_reduction/tsne_python/tsne.py
+++ studies/dimensionality_reduction/tsne_python/tsne.py
@@ -122,7 +122,7 @@ def tsne(X=np.array([]), no_dims=2, initial_dims=50, perplexity=30.0):
     # Initialize variables
     X = pca(X, initial_dims).real
     (n, d) = X.shape
-    max_iter = 1000
+    max_iter = 1000           # EU MUDEI ISSO AQUI
     initial_momentum = 0.5
     final_momentum = 0.8
     eta = 500
